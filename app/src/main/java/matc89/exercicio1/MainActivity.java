819c299129d591c455624feac3d2fbf2659b469f
unknown <mdrick19@gmail.com>
2019-09-08 19:19:39 -0300

    Tudo de vez =D

--- app/src/main/java/matc89/exercicio1/MainActivity.java
+++ app/src/main/java/matc89/exercicio1/MainActivity.java
@@ -2,6 +2,7 @@ package matc89.exercicio1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -9,22 +10,32 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
-    private EditText editText;
-    private TextView textView;
-    TextView label;
-    EditText mensagem;
+    private EditText input;
+    private TextView label;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.label = (TextView)findViewById(R.id.labelMensagem);
-        this.mensagem = (EditText)findViewById(R.id.editNome);
+        this.input = (EditText)findViewById(R.id.editNome);
     }
 
-    protected void onPress(View minhaView){
-        
+    public void onPress(View minhaView){
+        this.label.setText("Alô, "+this.input.getText()+"!");
+    }
 
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("inputText", this.input.getText().toString());
+        outState.putString("labelText", (String) this.label.getText().toString());
+    }
 
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState){
+
+        this.input.setText(savedInstanceState.getString("inputText"));
+        this.label.setText(savedInstanceState.getString("labelText"));
     }
 }
